7904867231c01e4ec1c47cd010e1c95d062dda89
Michelle Mattox <52865384+milmattox@users.noreply.github.com>
2022-06-29 15:50:20 +0000

    Money Updates

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import random
 window = tk.Tk()
 window.title("Vending Machine")
 window.geometry("500x300")
-#initialize global variables
-smarties,cookies,snickers,cheetos,reeses =0,0,0,0,0
 
 itemName = ['smarties','snickers','cheetos','cookies','musketeers']
 itemPrice = [1.00, 1.50, 1.25, 1.50, 1.50]
@@ -14,15 +12,20 @@ itemQuantity = [0,0,0,0,0]
 itemQuantityLabel = [0,0,0,0,0]
 itemPriceLabel = [0,0,0,0,0]
 itemButton = [0,0,0,0,0]
+orderItems = []
+orderItemsNum = 0
 money = 0
 def setQuantities():  #set the starting inventory
     for i in range(len(itemName)):
         itemQuantity[i] =random.randint(1,10)
         
 def myOrder(itemnum): #place an order
+  global money
   if money > 0:
     if itemQuantity[itemnum] > 0:
       itemQuantity[itemnum]-=1
+      money = money - itemPrice[itemnum]
+      myMoneyLabel.config(text="${:,.2f}".format(money))
     else:
       itemOutLabel.config(text = "This item out: "+ itemName[itemnum])
   else: 
@@ -33,8 +36,7 @@ def myMoney(): #enter money
   global money
   try:
     money = float(e.get())
-    myLabel3 = tk.Label(text = "${:,.2f}".format(money))
-    myLabel3.grid(row=1,column=4)
+    myMoneyLabel.config(text="${:,.2f}".format(money))
   except:
     itemOutLabel.config(text = "Enter some cash!")
     
@@ -43,6 +45,8 @@ e = tk.Entry(borderwidth=2, width=15) #enter money
 #e.insert(0,"Enter amount")
 cashButton = tk.Button(text="Enter Cash", command=myMoney, bg="lightgreen")
 itemOutLabel = tk.Label(text = "", fg="red")
+myMoneyLabel = tk.Label(text = "${:,.2f}".format(money))
+myMoneyLabel.grid(row=1,column=4)
 #place instructions on grid
 ack1.grid(row=0,column=0, columnspan=4)
 itemOutLabel.grid(row=5,column=0,columnspan=3)
