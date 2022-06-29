df78833e6f76dbd7ffd6abfe0a9b054cd5906a9c
Michelle Mattox <52865384+milmattox@users.noreply.github.com>
2022-06-29 14:25:41 +0000

    put in loops for various items

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ itemPrice = [1.00, 1.50, 1.25, 1.50, 1.50]
 itemQuantity = [0,0,0,0,0]
 itemQuantityLabel = [0,0,0,0,0]
 itemPriceLabel = [0,0,0,0,0]
-itemButton =[0,0,0,0,0]
+itemButton = [0,0,0,0,0]
+
 def setQuantities():  #set the starting inventory
     for i in range(len(itemName)):
         itemQuantity[i] =random.randint(1,10)
@@ -36,42 +37,32 @@ ack1.grid(row=0,column=0, columnspan=4)
 #place entry box on grid
 e.grid(row=1,column=2, columnspan=2)
 cashButton.grid(row=1,column=0, columnspan=2)
-#buttons
+#item Buttons
 itemButton[0] = tk.Button(text=itemName[0], command=lambda:myOrder(0), bg="lightblue")
 itemButton[1] = tk.Button(text=itemName[1], command=lambda:myOrder(1), bg="lightpink")
 itemButton[2] = tk.Button(text=itemName[2], command=lambda:myOrder(2), bg="lightyellow")
 itemButton[3] = tk.Button(text=itemName[3], command=lambda:myOrder(3), bg="orange")
 itemButton[4] = tk.Button(text=itemName[4], command=lambda:myOrder(4), bg="violet")
 #place buttons
-itemButton[0].grid(row=2,column=0)
-itemButton[1].grid(row=2,column=1)
-itemButton[2].grid(row=2, column=2)
-itemButton[3].grid(row=2, column=3)
-itemButton[4].grid(row=2, column=4)
+for i in range(len(itemButton)):
+  itemButton[i].grid(row=2,column=i)
+
 setQuantities()
 #quantity labels
-itemQuantityLabel[0] = tk.Label(text = itemQuantity[0])
-itemQuantityLabel[1] = tk.Label(text = itemQuantity[1])
-itemQuantityLabel[2] = tk.Label(text = itemQuantity[2])
-itemQuantityLabel[3] = tk.Label(text = itemQuantity[3])
-itemQuantityLabel[4] = tk.Label(text = itemQuantity[4])
+for i in range(len(itemQuantityLabel)):
+  itemQuantityLabel[i] = tk.Label(text = itemQuantity[i])
+
 #price labels
-itemPriceLabel[0] = tk.Label(text = "${:,.2f}".format(itemPrice[0]))
-itemPriceLabel[1] = tk.Label(text = "${:,.2f}".format(itemPrice[1]))
-itemPriceLabel[2] = tk.Label(text = "${:,.2f}".format(itemPrice[2]))
-itemPriceLabel[3] = tk.Label(text = "${:,.2f}".format(itemPrice[3]))
-itemPriceLabel[4] = tk.Label(text = "${:,.2f}".format(itemPrice[4]))
+for i in range(len(itemPriceLabel)):
+  itemPriceLabel[i] = tk.Label(text = "${:,.2f}".format(itemPrice[i]))
+
 #place price labels
-itemPriceLabel[0].grid(row=3,column=0)
-itemPriceLabel[1].grid(row=3,column=1)
-itemPriceLabel[2].grid(row=3,column=2)
-itemPriceLabel[3].grid(row=3,column=3)
-itemPriceLabel[4].grid(row=3,column=4)
+for i in range(len(itemPriceLabel)):
+  itemPriceLabel[i].grid(row=3,column=i)
+
 #place quantity labels
-itemQuantityLabel[0].grid(row=4,column=0)
-itemQuantityLabel[1].grid(row=4,column=1)
-itemQuantityLabel[2].grid(row=4,column=2)
-itemQuantityLabel[3].grid(row=4,column=3)
-itemQuantityLabel[4].grid(row=4,column=4)
+for i in range(len(itemPriceLabel)):
+  itemQuantityLabel[i].grid(row=4,column=i)
+
 
 tk.mainloop()
